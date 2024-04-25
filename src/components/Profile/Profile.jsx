45f9f7e8ad2a21cf11cb34b2a162e7493e5cfd0d
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-04-26 01:27:23 +0300

    add 1 task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+// import clsx from 'clsx';
+import css from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div>
+      <div className={css.boxinfo}>
+        <img className={css.avatar} src={image} alt={name} width={270} />
+        <p className={css.title}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.listitem}>
+          <span>Followers</span>
+          <b>
+            <span>{stats.followers}</span>
+          </b>
+        </li>
+        <li className={css.listitem}>
+          <span>Views</span>
+          <b>
+            <span>{stats.views}</span>
+          </b>
+        </li>
+        <li className={css.listitem}>
+          <span>Likes</span>
+          <b>
+            <span>{stats.likes}</span>
+          </b>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
